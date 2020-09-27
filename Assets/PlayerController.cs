762e3d30fa69549b44e55f0039c11d7d28dd7642
SteeelNerve <georgiy.baulin@mail.ru>
2020-09-27 17:24:23 +0300

    First game setup

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+    public Rigidbody2D rb;
+    public Animator anim;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+            anim.SetBool("running", true);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+            anim.SetBool("running", true);
+        } else {
+            anim.SetBool("running", false);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 10);
+        }
+    }
+}
