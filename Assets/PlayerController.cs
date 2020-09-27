db9724e436090a8b54028dbdbc5139c283a3634f
SteeelNerve <georgiy.baulin@mail.ru>
2020-09-27 23:37:32 +0300

    Add jump animation

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,38 +1,80 @@
 ﻿using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Animator anim;
+    private enum State
+    {
+        idle,
+        running,
+        jumping,
+        falling
+    }
+    private State state = State.idle;
+    private Collider2D coll;
+    [SerializeField] private LayerMask ground;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
     }
-    
-    public Rigidbody2D rb;
-    public Animator anim;
+        
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        float hDirection = (Input.GetAxis("Horizontal"));
+
+        if (hDirection < 0)
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
-            anim.SetBool("running", true);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (hDirection > 0)
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
-            anim.SetBool("running", true);
-        } else {
-            anim.SetBool("running", false);
         }
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 10);
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+            state = State.jumping;
+        }
+
+        VelocityState();
+        anim.SetInteger("state", (int)state);
+    }
+    private void VelocityState()
+    {
+        if(state == State.jumping)
+        {
+            if(rb.velocity.y < .1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if(state == State.falling)
+        {
+            if (coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if(Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
         }
     }
 }
+
